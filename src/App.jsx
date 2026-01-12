9b150b8c5c9dd3d10e24530b13444635ff23d11c
Francisco Monteiro <franciscomedeirosmonteiro@gmail.com>
2026-01-12 20:50:48 -0300

    Add Vercel Analytics Requirements to Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Analytics } from "@vercel/analytics/react";
 import { translations } from './translations';
 
 function App() {
@@ -206,6 +207,7 @@ function App() {
           {t.clear_button}
         </button>
       </div>
+      <Analytics />
     </div>
   );
 }
